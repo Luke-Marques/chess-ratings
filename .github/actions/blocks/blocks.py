34d228a2adfc497859bc29cc893b8040075edaa6
Luke Marques <74625391+Luke-Marques@users.noreply.github.com>
2024-03-22 15:54:25 +0000

    Refactor Prefect GitHub Actions/Workflows (#3)
    * chore: set default repository name to chess-ratings from prefect-gcp-template
    
    * fix: change gcp region in deploy-container gh action to europe-north1
    
    * chore: update default names of prefect deployment/queue to reference project name
    
    * chore: update default values of block names to reference project name in deploy-flows gh action
    
    * chore: update default values of block names to reference project name in gh prefect workflows
    
    * chore: update default values for prefect deployment/queue names in gh prefect workflows to reference project name and dev branch
    
    * chore: change name of main prefect gh workflow
    
    * chore: changed triggers for deploy-flows prefect workflow
    
    * chore: update todo list status
    
    * chore: add prefect complete gh workflow pipeline, replacing old quick-start.yml workflow
    
    * chore: changed name of gh prefect gcp sa credentials json contents to PREFECT_GCP_CREDENTIALS from GCP_CREDENTIALS to allow for differentiation between prefect and other gcp sa credentials
    
    * fix: changed the blocks.py file to take an additional argument for the gcp-credentials prefect block name, allowing a different name to the github and cloudrunjob blocks. Updated the gh action to reflect this change

--- .github/actions/blocks/blocks.py
+++ .github/actions/blocks/blocks.py
@@ -10,12 +10,13 @@ from prefect_gcp.credentials import GcpCredentials
 
 from prefect.filesystems import GitHub
 
-REPO = "https://github.com/Luke-Marques/prefect-gcp-template"
+REPO = "https://github.com/Luke-Marques/chess-ratings"
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--branch", default="main")
 parser.add_argument("-r", "--repo", default=REPO)
 
 parser.add_argument("-n", "--block-name", default="default")
+parser.add_argument("-g", "--gcp-creds-block-name", default="default")
 parser.add_argument("-i", "--image")
 parser.add_argument("--region", default="europe-north1")
 
@@ -27,7 +28,7 @@ gh.save(args.block_name, overwrite=True)
 block = CloudRunJob(
     image=args.image,
     region=args.region,
-    credentials=GcpCredentials.load(args.block_name),
+    credentials=GcpCredentials.load(args.gcp_creds_block_name),
     cpu=1,
     timeout=3600,
 )
