69a4a1ced50fa49e8c7a6d8e99b69837029712a9
Luke Marques <74625391+Luke-Marques@users.noreply.github.com>
2024-03-28 00:48:59 +0000

    fix: change name of gcs bucket block to match that created in blocks.py script

--- prefect/flows/ingest_web_to_gcs.py
+++ prefect/flows/ingest_web_to_gcs.py
@@ -9,7 +9,6 @@ import pandas as pd
 import polars as pl
 import requests
 from prefect_gcp.cloud_storage import GcsBucket
-from prefect_gcp.credentials import GcpCredentials
 
 from prefect import flow, task
 from prefect.tasks import task_input_hash
@@ -155,7 +154,7 @@ def write_ratings_data_to_gcs(df: pl.DataFrame, out_path: Path) -> Path:
         end=" ",
     )
     df: pd.DataFrame = df.to_pandas()  # prefect-gcp requires pandas dataframe
-    gcs_bucket_block = GcsBucket.load("chess-ratings-datalake_fide-chess-ratings")
+    gcs_bucket_block = GcsBucket.load("chess-ratings-dev")
     gcs_bucket_block.upload_from_dataframe(
         df=df, to_path=out_path, serialization_format="parquet"
     )
@@ -166,7 +165,7 @@ def write_ratings_data_to_gcs(df: pl.DataFrame, out_path: Path) -> Path:
 @task()
 def check_if_file_exists_in_gcs(file_path: Path) -> bool:
     """Determine if a filepath already exists in a GCS Bucket."""
-    gcs_bucket_block = GcsBucket.load("chess-ratings-datalake_fide-chess-ratings")
+    gcs_bucket_block = GcsBucket.load("chess-ratings-dev")
     blobs = gcs_bucket_block.list_blobs()
     paths = [Path(blob.name) for blob in blobs]
     if file_path in paths:
