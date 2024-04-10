0abf70037457f80dddd5ec4255baa4a594d31f1e
Luke Marques <74625391+Luke-Marques@users.noreply.github.com>
2024-04-10 14:01:28 +0100

    Refactor game_format.py: add documentation for GameFormat enum class

--- prefect/flows/utils/game_format.py
+++ prefect/flows/utils/game_format.py
@@ -2,6 +2,10 @@ from enum import StrEnum
 
 
 class GameFormat(StrEnum):
+    """
+    Enumeration class representing different FIDE chess game-formats.
+    """
+
     STANDARD = "standard"
     RAPID = "rapid"
     BLITZ = "blitz"
