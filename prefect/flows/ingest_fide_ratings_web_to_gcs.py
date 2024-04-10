ab3fc6d17d80b6d7bdd9ed1a404464bf2c50d2e0
Luke Marques <74625391+Luke-Marques@users.noreply.github.com>
2024-04-10 13:54:04 +0100

    Refactor FIDE chess ratings data ingestion script: add docstrings to functions, rename ratings preprocessing function to clean_ratings_data to match other scripts

--- prefect/flows/ingest_fide_ratings_web_to_gcs.py
+++ prefect/flows/ingest_fide_ratings_web_to_gcs.py
@@ -20,7 +20,17 @@ from prefect import flow, task
 def add_missing_columns(
     df: pl.DataFrame | pl.LazyFrame, required_columns: Iterable[str]
 ) -> pl.DataFrame | pl.LazyFrame:
-    """Creates empty required columns if they don't already exist in the dataframe."""
+    """
+    Adds missing columns to a Polars DataFrame or LazyFrame.
+
+    Args:
+        df (pl.DataFrame | pl.LazyFrame): The input Polars DataFrame or LazyFrame.
+        required_columns (Iterable[str]): The list of required column names.
+
+    Returns:
+        pl.DataFrame | pl.LazyFrame: 
+            The Polars DataFrame or LazyFrame with missing columns added.
+    """
     df = df.with_columns(
         [
             pl.lit(None).cast(pl.Utf8).alias(col)
@@ -34,8 +44,19 @@ def add_missing_columns(
 @task
 def generate_fide_download_url(year: int, month: int, game_format: GameFormat) -> str:
     """
-    Generate a download url for FIDE chess ratings data for a given year, month, and
-    game format.
+    Generate the download URL for FIDE chesss ratings based on the given year, month, 
+    and FIDE game-format.
+
+    Args:
+        year (int): The year of the ratings.
+        month (int): The month of the ratings.
+        game_format (GameFormat): The format of the game (e.g., standard, rapid, blitz).
+
+    Returns:
+        str: The download URL for the FIDE ratings.
+
+    Raises:
+        ValueError: If the month or year is invalid.
     """
     check_valid_month(month)
     check_valid_year(year)
@@ -47,8 +68,20 @@ def generate_fide_download_url(year: int, month: int, game_format: GameFormat) -
     return url
 
 
+@task
 def generate_file_path(year: int, month: int, game_format: GameFormat) -> Path:
-    """Generate a file path for a month's FIDE chess ratings data, as a Path object."""
+    """
+    Generate a file path for FIDE chess ratings based on the given year, month, and FIDE
+    game-format.
+
+    Args:
+        year (int): The year of the ratings.
+        month (int): The month of the ratings.
+        game_format (GameFormat): The FIDE game-format.
+
+    Returns:
+        Path: The file path for the FIDE chess ratings.
+    """
     # Generate file name
     file_name = Path(f"fide_chess_ratings_{year}_{month:02d}_{game_format.value}")
 
@@ -60,7 +93,15 @@ def generate_file_path(year: int, month: int, game_format: GameFormat) -> Path:
 
 @task(log_prints=True, retries=3, cache_result_in_memory=False)
 def parse_xml_url_to_dataframe(url: str) -> pl.DataFrame:
-    """Read an ZIP compressed XML file to a Polars DataFrame from a URL, via Pandas."""
+    """
+    Parses an XML file from the given URL, via Pandas, and returns a Polars DataFrame.
+
+    Args:
+        url (str): The URL of the XML file.
+
+    Returns:
+        pl.DataFrame: A Polars DataFrame containing the parsed data from the XML file.
+    """
     df: pl.DataFrame = pl.from_pandas(pd.read_xml(url))
     return df
 
@@ -70,8 +111,16 @@ def extract_ratings_data(
     year: int, month: int, game_format: GameFormat
 ) -> pl.DataFrame:
     """
-    Extract FIDE ratings data for given year, month, and game format, from
-    compressed XML file to Polars DataFrame.
+    Prefect sub-flow extracts FIDE chess ratings data for a given year, month, and FIDE 
+    game-format.
+
+    Args:
+        year (int): The year of the ratings data.
+        month (int): The month of the ratings data.
+        game_format (GameFormat): The FIDE game-format of the ratings data.
+
+    Returns:
+        pl.DataFrame: The extracted FIDE chess ratings data as a Polars DataFrame.
     """
     # create download url
     print(
@@ -89,8 +138,21 @@ def extract_ratings_data(
 
 
 @task
-def preprocess_ratings_data(df: pl.DataFrame, year: int, month: int) -> pl.DataFrame:
-    """Initial preprocessing of raw FIDE chess ratings dataset."""
+def clean_ratings_data(df: pl.DataFrame, year: int, month: int) -> pl.DataFrame:
+    """
+    Clean a Polars DataFrame containing FIDE chess ratings data by adding missing 
+    columns, renaming columns, converting data types, and adding columns corresponding 
+    to the year/month that the ratings relate to.
+
+    Args:
+        df (pl.DataFrame): The input Polars DataFrame containing the ratings data.
+        year (int): The year of the ratings period.
+        month (int): The month of the ratings period.
+
+    Returns:
+        pl.DataFrame: The cleaned Polars DataFrame of FIDE chess ratings.
+
+    """
     # add missing columns to dataframe
     required_columns = ["foa_title"]
     df: pl.DataFrame = add_missing_columns(df, required_columns)
@@ -123,7 +185,18 @@ def preprocess_ratings_data(df: pl.DataFrame, year: int, month: int) -> pl.DataF
 @task
 def validate_ratings_data(df: pl.DataFrame) -> None:
     """
-    Validate ratings records in FIDE chess ratings dataset using Patito data model.
+    Validates the FIDE chess ratings data in the given Polars DataFrame using Patito 
+    data model.
+
+    Args:
+        df (pl.DataFrame): The Polars DataFrame containing the FIDE chess ratings data.
+
+    Returns:
+        None
+
+    Raises:
+        patito.exceptions.DataFrameValidationError: 
+            If the given Polars DataFrame does not match the Patito data model/schema.
     """
     ChessRating.validate(df)
 
@@ -138,8 +211,30 @@ def ingest_single_month_fide_ratings_web_to_gcs(
     overwrite_existing: bool = False,
 ) -> Tuple[pl.DataFrame, Path] | None:
     """
-    Sub-flow for the extraction, pre-processing, and writing of FIDE chess ratings
-    data to GCS, for a given of date (year and month) and game format.
+    Prefect sub-flow which ingests FIDE chess ratings data for a given year and month 
+    from the web, converts the downloaded XML file to a Polars DataFrame, cleans the 
+    DataFrame, and writes the cleaned DataFrame as a parquet file to a GCS bucket and/or 
+    locally.
+
+    Args:
+        year (int): The year of the ratings data.
+        month (int): The month of the ratings data.
+        game_format (GameFormat): 
+            The format of the chess games as a GameFormat object (e.g., 
+            GameFormat.STANDARD, GameFormat.RAPID, GameFormat.BLITZ).
+        gcs_bucket_block_name (str, optional): 
+            The name of the GCS bucket Prefect block. Defaults to "chess-ratings-dev".
+        store_local (bool, optional): 
+            Whether to store the cleaned ratings dataset locally. Defaults to False.
+        overwrite_existing (bool, optional): 
+            Whether to overwrite the existing ratings dataset in GCS if it already 
+            exists. Defaults to False.
+
+    Returns:
+        Tuple[pl.DataFrame, Path] | None: A tuple containing the cleaned ratings dataset 
+        as a Polars DataFrame and the file path where it is stored in GCS/locally. 
+        Returns None if the ratings dataset already exists in GCS and overwrite_existing 
+        is False.
     """
     # generate file path for cleaned ratings dataset
     out_path: Path = generate_file_path(year, month, game_format)
@@ -158,7 +253,7 @@ def ingest_single_month_fide_ratings_web_to_gcs(
     print(df.head())
 
     # clean ratings dataset
-    df = preprocess_ratings_data(df, year, month)
+    df = clean_ratings_data(df, year, month)
     print("Cleaned data:")
     print(df.head())
 
@@ -187,9 +282,31 @@ def ingest_fide_ratings_web_to_gcs(
     overwrite_existing: bool = True,
 ) -> None:
     """
-    Parent-flow for the extraction, pre-processing, and writing of FIDE chess ratings
-    data to GCS, across a range of dates (years and months) and game formats.
+    Prefect parent-flow which ingests FIDE chess ratings data from the web across a 
+    range of years/months/game-format combinations, converts the downloaded XML files to 
+    Polars DataFrames, cleans the DataFrames, and writes the cleaned DataFrames to 
+    parquet files in a GCS bucket and/or locally.
+
+    Args:
+        year (int | Iterable[int]): 
+            The year(s) for which to ingest the ratings. Defaults to the current year.
+        month (int | Iterable[int]): 
+            The month(s) for which to ingest the ratings. Defaults to the current month.
+        game_format (str | Iterable[str]): 
+            The FIDE game-format(s) for which to ingest the ratings. Defaults to "all", 
+            representing the standard, rapid, and blitz game-formats.
+        gcs_bucket_block_name (str): 
+            The name of the GCS bucket Prefect block where the ratings data will be 
+            written to.
+        store_local (bool): 
+            Whether to store the ratings locally in addition to GCS. Defaults to False.
+        overwrite_existing (bool): 
+            Whether to overwrite existing ratings in GCS. Defaults to True.
+
+    Returns:
+        None
     """
+    
     # convert int year/month values to lists
     if isinstance(year, int):
         year = [year]
