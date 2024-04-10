a036f29f83bbf22c4186accfcac88be7c221fb54
Luke Marques <74625391+Luke-Marques@users.noreply.github.com>
2024-04-10 14:07:45 +0100

    Refactor chess_ratings_data_model.py: add docstring to ChessRating Patito data model

--- prefect/flows/utils/chess_ratings_data_model.py
+++ prefect/flows/utils/chess_ratings_data_model.py
@@ -7,7 +7,31 @@ from pydantic import validator
 
 
 class ChessRating(pt.Model):
-    """Data model for validation of individual ratings datasets."""
+    """
+    Patito data model/schema which represents a FIDE chess rating entry.
+
+    Attributes:
+        fide_id (int): The FIDE ID of the player.
+        player_name (str, optional): The name of the player.
+        fide_federation (str, optional): The FIDE federation of the player.
+        sex (int, optional): The sex of the player.
+        title (str, optional): The title of the player.
+        w_title (str, optional): The women's title of the player.
+        o_title (str, optional): The open title of the player.
+        foa_title (str, optional): The FIDE Online Arena title of the player.
+        rating (int): The rating of the player.
+        game_count (int): The number of games played by the player.
+        k (int): The K-factor used for rating calculation.
+        birth_year (int, optional): The birth year of the player.
+        flag (str, optional): The flag of the player.
+        period_year (int): The year of the rating period.
+        period_month (int): The month of the rating period.
+
+    Methods:
+        within_date_range(cls, value): 
+            Validates that the birth year is within a valid range.
+
+    """
 
     fide_id: int = pt.Field(unique=True)
     player_name: Optional[str]
