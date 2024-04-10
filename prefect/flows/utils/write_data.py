05b1b15363652975354ec0ca5c32129663185abd
Luke Marques <74625391+Luke-Marques@users.noreply.github.com>
2024-04-10 14:00:20 +0100

    Refactor write_data.py: add docstrings to functions and add gcs_bucket_block_name parameter to check_if_file_exists_in_gcs function to improve extensibility (this defaults to "chess-ratings-dev")

--- prefect/flows/utils/write_data.py
+++ prefect/flows/utils/write_data.py
@@ -8,7 +8,17 @@ from prefect_gcp import GcsBucket
 
 @task(log_prints=True, cache_result_in_memory=False, persist_result=False)
 def write_to_local(df: pl.DataFrame, out_path: Path) -> Path:
-    """Write Polars DataFrame to local file."""
+    """
+    Writes a Polars DataFrame to a local parquet file, whilst ensuring parent-directory
+    exists.
+
+    Args:
+        df (pl.DataFrame): The Polars DataFrame to be written.
+        out_path (Path): The path to the output parquet file.
+
+    Returns:
+        Path: The path to the output parquet file.
+    """
     out_path.parent.mkdir(parents=True, exist_ok=True)
     df.write_parquet(out_path)
     return out_path
@@ -18,7 +28,19 @@ def write_to_local(df: pl.DataFrame, out_path: Path) -> Path:
 def write_to_gcs(
     df: pl.DataFrame, out_path: Path, gcs_bucket_block_name: str = "chess-ratings-dev"
 ) -> Path:
-    """Write Polars DataFrame to parquet file in GCS bucket."""
+    """
+    Writes a Polars DataFrame to a parquet file in a Google Cloud Storage (GCS) bucket
+    using the specified output path and GCS bucket Prefect block name.
+
+    Args:
+        df (pl.DataFrame): The Polars DataFrame to be written to GCS.
+        out_path (Path): The output path in GCS where the DataFrame will be stored.
+        gcs_bucket_block_name (str, optional):
+            The name of the GCS bucket Prefect block. Defaults to "chess-ratings-dev".
+
+    Returns:
+        Path: The output path where the DataFrame was stored in GCS.
+    """
     df: pd.DataFrame = df.to_pandas()  # prefect-gcp requires pandas dataframe
     gcs_bucket_block = GcsBucket.load(gcs_bucket_block_name)
     gcs_bucket_block.upload_from_dataframe(
@@ -28,9 +50,19 @@ def write_to_gcs(
 
 
 @task(retries=3)
-def check_if_file_exists_in_gcs(file_path: Path) -> bool:
-    """Determine if a filepath already exists in a GCS Bucket."""
-    gcs_bucket_block = GcsBucket.load("chess-ratings-dev")
+def check_if_file_exists_in_gcs(
+    file_path: Path, gcs_bucket_block_name: str = "chess-ratings-dev"
+) -> bool:
+    """
+    Check if a file exists in a Google Cloud Storage (GCS) bucket.
+
+    Args:
+        file_path (Path): The path of the file to check.
+
+    Returns:
+        bool: True if the file exists in the GCS bucket, False otherwise.
+    """
+    gcs_bucket_block = GcsBucket.load(gcs_bucket_block_name)
     blobs = gcs_bucket_block.list_blobs()
     paths = [Path(blob.name) for blob in blobs]
     if file_path in paths:
