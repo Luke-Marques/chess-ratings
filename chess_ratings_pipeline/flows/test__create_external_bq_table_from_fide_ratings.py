4b97f820a914341049fe8a00b87f65b99f56f2fa
Luke Marques <74625391+Luke-Marques@users.noreply.github.com>
2024-04-25 14:32:24 +0100

    fix: modify test flow to use fide ratings data files

--- chess_ratings_pipeline/flows/test__create_external_bq_table_from_fide_ratings.py
+++ chess_ratings_pipeline/flows/test__create_external_bq_table_from_fide_ratings.py
@@ -6,14 +6,16 @@ from prefect_gcp import GcpCredentials, GcsBucket
 
 
 @flow(log_prints=True)
-def test__create_external_bq_table_from_cdc_stats():
+def test__create_external_bq_table_from_fide_ratings():
     gcp_credentials_block = GcpCredentials.load("gcp-creds-chess-ratings")
     gcs_bucket_block = GcsBucket.load("chess-ratings-dev")
-    cdc_stats_uri = f"gs://{gcs_bucket_block.bucket}/data/chess_dot_com/player_game_stats/chess*/*/*.parquet"
+    cdc_stats_uri = (
+        f"gs://{gcs_bucket_block.bucket}/data/fide_ratings/standard/*.parquet"
+    )
     create_external_bq_table_from_gcs_files(
         gcs_file_uris=cdc_stats_uri,
         dataset="chess_ratings",
-        table="landing_cdc__chess_stats",
+        table="landing_fide__ratings_test",
         gcp_credentials=gcp_credentials_block,
         project="fide-chess-ratings",
     )
