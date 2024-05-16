6c1a426dd9060928f3901a844bc39face029d64e
Luke Marques <74625391+Luke-Marques@users.noreply.github.com>
2024-05-16 22:34:42 +0100

    fix: fixed parameters for dbt test flow

--- chess_ratings_pipeline/flows/build_all_dbt_models.py
+++ chess_ratings_pipeline/flows/build_all_dbt_models.py
@@ -5,8 +5,6 @@ from prefect import flow
 from prefect.logging import get_logger
 from prefect_dbt.cli.commands import DbtCoreOperation
 
-from chess_ratings_pipeline.core.integrations.dbt.dbt_core import dbt_build
-
 
 @flow
 def trigger_dbt_flow(project_dir: Path, profiles_dir: Path, commands: List[str]):
@@ -22,7 +20,7 @@ def trigger_dbt_flow(project_dir: Path, profiles_dir: Path, commands: List[str])
 def build_all_dbt_models():
     logger = get_logger()
     logger.info("Running dbt models via dbt core...")
-    dbt_result: List[str] = dbt_build(
+    dbt_result: List[str] = trigger_dbt_flow(
         project_dir=Path("dbt"), profiles_dir=Path("dbt"), commands=["pwd", "dbt run"]
     )
     logger.info(f"dbt result: {dbt_result}")
