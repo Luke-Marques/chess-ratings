de2ca5f44d7b2c04d3ec63d9fbece06e06b4465a
Luke Marques <74625391+Luke-Marques@users.noreply.github.com>
2024-05-16 01:33:07 +0100

    Merged .github directory from dbt-integration branch

--- .github/actions/blocks/blocks.py
+++ .github/actions/blocks/blocks.py
@@ -5,10 +5,10 @@ python blocks.py -b $GITHUB_REF_NAME -r "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" 
 
 import argparse
 
+from prefect.filesystems import GitHub
+from prefect_gcp import GcsBucket
 from prefect_gcp.cloud_run import CloudRunJob
 from prefect_gcp.credentials import GcpCredentials
-
-from prefect.filesystems import GitHub
 
 # parse command line arguments
 REPO = "https://github.com/Luke-Marques/chess-ratings"
@@ -17,9 +17,13 @@ parser.add_argument("-b", "--branch", default="main")
 parser.add_argument("-r", "--repo", default=REPO)
 parser.add_argument("-n", "--block-name", default="default")
 parser.add_argument("-g", "--gcp-creds-block-name", default="default")
+parser.add_argument("-p", "--bucket-name", default="default")
 parser.add_argument("-i", "--image")
-parser.add_argument("--region", default="europe-north1")
+parser.add_argument("--region", default="europe-west1")
 args = parser.parse_args()
+
+# load GCP credentials block
+gcp_credentials = GcpCredentials.load(args.gcp_creds_block_name)
 
 # create GitHub block and save to Prefect Cloud
 github_block = GitHub(repository=args.repo, reference=args.branch)
@@ -29,8 +33,15 @@ github_block.save(args.block_name, overwrite=True)
 cloud_run_job_block = CloudRunJob(
     image=args.image,
     region=args.region,
-    credentials=GcpCredentials.load(args.gcp_creds_block_name),
-    cpu=1,
+    credentials=gcp_credentials,
+    cpu=8,
+    memory=32,
+    memory_unit="Gi",
     timeout=3600,
+    max_retries=0,
 )
 cloud_run_job_block.save(args.block_name, overwrite=True)
+
+# create GCP GCS Bucket block and save to Prefect Cloud
+gcs_bucket_block = GcsBucket(bucket=args.bucket_name, credentials=gcp_credentials)
+gcs_bucket_block.save(args.block_name, overwrite=True)
