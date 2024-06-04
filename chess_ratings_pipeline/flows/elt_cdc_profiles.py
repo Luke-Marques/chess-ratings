032134a63458798a080fa90970eaa99a677f8c16
Luke Marques <74625391+Luke-Marques@users.noreply.github.com>
2024-06-04 17:38:52 +0200

    chore: remove cluttering logging messages and use flow start/end logging message functions

--- chess_ratings_pipeline/flows/elt_cdc_profiles.py
+++ chess_ratings_pipeline/flows/elt_cdc_profiles.py
@@ -1,13 +1,13 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from typing import List, Literal
 
 import polars as pl
+from google.cloud import bigquery
 from prefect import flow
 from prefect.logging import get_run_logger
 from prefect.runtime import flow_run
 from prefect_gcp import GcpCredentials
 from prefect_gcp.cloud_storage import GcsBucket
-from google.cloud import bigquery
 
 from chess_ratings_pipeline.core.integrations.cdc.chess_title import ChessTitle
 from chess_ratings_pipeline.core.integrations.cdc.profiles import (
@@ -15,6 +15,7 @@ from chess_ratings_pipeline.core.integrations.cdc.profiles import (
     extract_titled_cdc_profiles,
     generate_cdc_profiles_file_path,
 )
+from chess_ratings_pipeline.core.integrations.dbt.dbt_core import trigger_dbt_flow
 from chess_ratings_pipeline.core.integrations.google_bigquery import (
     create_external_bq_table,
 )
@@ -22,7 +23,10 @@ from chess_ratings_pipeline.core.integrations.google_cloud_storage import (
     write_dataframe_to_gcs,
     write_dataframe_to_local,
 )
-from chess_ratings_pipeline.core.integrations.dbt.dbt_core import trigger_dbt_flow
+from chess_ratings_pipeline.core.logging_messages.logging_messages import (
+    generate_flow_end_log_message,
+    generate_flow_start_log_message,
+)
 
 
 def generate_extract_single_title_cdc_profiles_flow_name() -> str:
@@ -66,78 +70,42 @@ def generate_elt_cdc_profiles_flow_name() -> str:
 )
 def extract_single_title_cdc_profiles(
     chess_title: ChessTitle,
-    gcp_credentials_block: GcpCredentials,
     gcs_bucket_block: GcsBucket,
     store_local: bool,
     overwrite_existing: bool,
 ) -> None:
-    # Create Prefect info logger
-    logger = get_run_logger()
-
     # Log flow start message
+    logger = get_run_logger()
     start_time = datetime.now()
-    start_message = f"""Starting `elt_single_title_cdc_profiles` sub-flow at {start_time} (local time).
-    Inputs:
-        chess_title (ChessTitle): {chess_title}
-        gcp_credentials_block (GcpCredentials): {gcp_credentials_block}
-        gcs_bucket_block (GcsBucket): {gcs_bucket_block}
-        store_local (bool): {store_local}
-        overwrite_existing (bool): {overwrite_existing}"""
+    start_message: str = generate_flow_start_log_message(
+        "elt_single_title_cdc_profiles",
+        start_time,
+        chess_title,
+        gcs_bucket_block,
+        store_local,
+        overwrite_existing,
+    )
     logger.info(start_message)
 
     # Extract Chess.com player profiles for the specified ChessTitle
-    logger.info(
-        "Extracting Chess.com player profiles for "
-        f"{chess_title.value} titled players on "
-        f"{datetime.now().date} at {datetime.now().time}..."
-    )
     cdc_profiles: pl.DataFrame = extract_titled_cdc_profiles(chess_title)
-    logger.info(f"Extracted {cdc_profiles.shape[0]} player profiles.")
-    logger.info(f"Profiles DataFrame: \n\t{cdc_profiles}")
 
     # Apply initial cleaning/pre-processing to player profiles DataFrame
-    logger.info(
-        f"Cleaning Chess.com {chess_title.value} titled player profiles DataFrame..."
-    )
-    cdc_profiles: pl.DataFrame = clean_cdc_profiles(cdc_profiles)
-    logger.info(
-        f"Cleaned Chess.com {chess_title.value} titled player profiles DataFrame: "
-        f"\n\t{cdc_profiles}"
-    )
-
-    # Generate destination parquet file path for writing of player profiles DataFrame
-    logger.info(
-        "Generating destination parquet file path for writing of player profiles "
-        "DataFrame..."
-    )
-    destination: str = generate_cdc_profiles_file_path(chess_title)
-    logger.info(
-        f"Generated destination parquet file path for Chess.com {chess_title.value} "
-        f"titled player profiles: {destination}"
-    )
+    cdc_profiles_clean: pl.DataFrame = clean_cdc_profiles(cdc_profiles)
 
     # Write player profiles DataFrame to parquet file in GCS bucket and/or locally
-    logger.info(
-        f"Writing cleaned Chess.com {chess_title.value} titled player profiles "
-        f"DataFrame to GCS bucket {gcs_bucket_block.bucket} at {destination}..."
-    )
+    destination: str = generate_cdc_profiles_file_path(chess_title)
     write_dataframe_to_gcs(
-        cdc_profiles, destination, gcs_bucket_block, overwrite_existing
+        cdc_profiles_clean, destination, gcs_bucket_block, overwrite_existing
     )
-    logger.info("Finished writing player profiles DataFrame to GCS bucket.")
     if store_local:
-        logger.info(
-            f"Writing cleaned Chess.com {chess_title.value} titled player profiles "
-            f"DataFrame to {destination}..."
-        )
-        write_dataframe_to_local(cdc_profiles, destination, overwrite_existing)
-        logger.info("Finished writing player profiles DataFrame locally.")
+        write_dataframe_to_local(cdc_profiles_clean, destination, overwrite_existing)
 
     # Log flow end message
     end_time = datetime.now()
-    time_taken: timedelta = end_time - start_time
-    end_message = f"""Finished `elt_single_title_cdc_profiles` sub-flow at {end_time} (local time).
-    Time taken: {time_taken}."""
+    end_message: str = generate_flow_end_log_message(
+        "extract_single_title_cdc_profiles", start_time, end_time
+    )
     logger.info(end_message)
 
 
@@ -149,18 +117,18 @@ def load_cdc_profiles_to_bq_external_table(
     bq_dataset_name: str = "chess_ratings",
     bq_table_name: str = "landing_cdc__profiles",
 ) -> str:
-    # Create Prefect logger
-    logger = get_run_logger()
-
     # Log flow start message
+    logger = get_run_logger()
     start_time = datetime.now()
-    start_message = f"""Starting `load_cdc_profiles_to_bq_external_table` flow at {start_time} (local time).
-    Inputs:
-        gcp_credentials_block (GcpCredentials): {gcp_credentials_block}
-        gcs_bucket_block (GcsBucket): {gcs_bucket_block}
-        project (str): {project}
-        bq_dataset_name (str): {bq_dataset_name}
-        bq_table_name (str): {bq_table_name}"""
+    start_message: str = generate_flow_start_log_message(
+        "load_cdc_profiles_to_bq_external_table",
+        start_time,
+        gcp_credentials_block,
+        gcs_bucket_block,
+        project,
+        bq_dataset_name,
+        bq_table_name,
+    )
     logger.info(start_message)
 
     # Get list of parent directories containing Chess.com player profiles Parquet files
@@ -206,9 +174,9 @@ def load_cdc_profiles_to_bq_external_table(
 
     # Log flow end message
     end_time = datetime.now()
-    time_taken: timedelta = end_time - start_time
-    end_message = f"""Finished `load_cdc_profiles_to_bq_external_table` flow at {end_time} (local time).
-        Time taken: {time_taken}."""
+    end_message: str = generate_flow_end_log_message(
+        "load_cdc_profiles_to_bq_external_table", start_time, end_time
+    )
     logger.info(end_message)
 
 
@@ -221,78 +189,36 @@ def elt_cdc_profiles(
     overwrite_existing: bool = True,
     bq_dataset_name: str = "chess_ratings",
     bq_table_name: str = "landing_cdc__profiles",
-    dbt_job_id: int = 638701,
 ) -> None:
-    """
-    Extract, load, and transform Chess.com player profiles for the specified chess
-    titles.
-
-    Args:
-        chess_titles (ChessTitle | List[ChessTitle] | Literal["all"], optional):
-            The chess titles to process. Defaults to "all".
-        gcp_credentials_block_name (str, optional):
-            The name of the GCP credentials Prefect block. Defaults to
-            "gcp-creds-chess-ratings".
-        gcs_bucket_block_name (str, optional):
-            The name of the GCS bucket Prefect block. Defaults to "chess-ratings-dev".
-        store_local (bool, optional):
-            Flag indicating whether to store the data locally. Defaults to False.
-        overwrite_existing (bool, optional):
-            Flag indicating whether to overwrite existing data. Defaults to True.
-        bq_dataset_name (str, optional):
-            The name of the BigQuery dataset. Defaults to "landing".
-
-    Returns:
-        None
-    """
-    # Create Prefect info logger
-    logger = get_run_logger()
-
     # Log flow start message
+    logger = get_run_logger()
     start_time = datetime.now()
-    start_message = f"""Starting `elt_cdc_profiles` flow at {start_time} (local time).
-    Inputs:
-        chess_titles (ChessTitle | List[ChessTitle] | Literal["all"]): {chess_titles}
-        gcp_credentials_block_name (str): {gcp_credentials_block_name}
-        gcs_bucket_block_name (str): {gcs_bucket_block_name}
-        store_local (bool): {store_local}
-        overwrite_existing (bool): {overwrite_existing}"""
+    start_message: str = generate_flow_start_log_message(
+        "elt_cdc_profiles",
+        start_time,
+        chess_titles,
+        gcp_credentials_block_name,
+        gcs_bucket_block_name,
+        store_local,
+        overwrite_existing,
+        bq_dataset_name,
+        bq_table_name,
+    )
     logger.info(start_message)
 
-    # If chess_titles is "all", set it to list of all ChessTitle objects
     if chess_titles == "all":
         chess_titles = list(ChessTitle)
-
-    # If chess_titles is not a list, convert it to a list
-    if not isinstance(chess_titles, list):
+    elif not isinstance(chess_titles, list):
         chess_titles = [chess_titles]
 
-    # Load GCP credentials Prefect block
-    logger.info(
-        f"Loading GCP credentials Prefect block {gcp_credentials_block_name}..."
-    )
+    # Load GCP and GCS Prefect blocks
     gcp_credentials_block: GcpCredentials = GcpCredentials.load(
         gcp_credentials_block_name
     )
-    logger.info(f"Loaded GCP credentials Prefect block {gcp_credentials_block_name}.")
-
-    # Load GCS bucket Prefect block
-    logger.info(f"Loading GCS bucket Prefect block {gcs_bucket_block_name}...")
     gcs_bucket_block: GcsBucket = GcsBucket.load(gcs_bucket_block_name)
-    logger.info(
-        f"Loaded GCS bucket Prefect block. Bucket name: {gcs_bucket_block.bucket}"
-    )
 
     # Extract Chess.com player profiles for each ChessTitle to GCS bucket
-    logger.info(
-        "Running Chess.com player profiles ELT sub-flow for each chess title "
-        "specified..."
-    )
     for chess_title in chess_titles:
-        logger.info(
-            f"Running Chess.com player profiles ELT sub-flow for Chess.com players "
-            f"with {chess_title.value} titles..."
-        )
         extract_single_title_cdc_profiles(
             chess_title,
             gcp_credentials_block,
@@ -301,15 +227,8 @@ def elt_cdc_profiles(
             overwrite_existing,
             return_state=True,
         )
-        logger.info(
-            f"Finished Chess.com player profiles ELT sub-flow for Chess.com players "
-            f"with {chess_title.value} titles."
-        )
 
     # Load Chess.com player profiles to BigQuery external table
-    logger.info(
-        f"Loading Chess.com player profiles to BigQuery external table {bq_dataset_name}.{bq_table_name}..."
-    )
     load_cdc_profiles_to_bq_external_table(
         gcp_credentials_block=gcp_credentials_block,
         gcs_bucket_block=gcs_bucket_block,
@@ -317,21 +236,15 @@ def elt_cdc_profiles(
         bq_table_name=bq_table_name,
         return_state=True,
     )
-    logger.info(
-        f"Finished loading Chess.com player profiles to BigQuery external table {bq_dataset_name}.{bq_table_name}."
-    )
 
     # Run dbt models via dbt Cloud job
-    logger.info("Running dbt models via dbt core...")
-    dbt_result: List[str] = trigger_dbt_flow(commands=["pwd", "dbt debug", "dbt build"])
-    logger.info("Finished running dbt models via dbt core.")
-    logger.info(f"dbt result: {dbt_result}")
+    _: List[str] = trigger_dbt_flow(commands=["pwd", "dbt debug", "dbt build"])
 
     # Log flow end message
     end_time = datetime.now()
-    time_taken: timedelta = end_time - start_time
-    end_message = f"""Finished `elt_cdc_profiles` flow at {end_time} (local time).
-    Time taken: {time_taken}."""
+    end_message: str = generate_flow_end_log_message(
+        "elt_cdc_profiles", start_time, end_time
+    )
     logger.info(end_message)
 
 
